2b960b0e078baed2c0da3e0f91298d3c9eb4e044
Persis Randolph <persis.randolph@adas-safe.com>
2025-08-09 12:22:08 -0500

    add fonts, remove base boilerplate content

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	devIndicators: {
+		buildActivity: false,
+		buildActivityPosition: "bottom-right",
+	},
 };
 
 export default nextConfig;
